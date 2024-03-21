ee1259b0ae69db9482791e773e5ca2e1459be55c
Karan Raj <karanrajgodansa2003@gmail.com>
2024-03-21 17:35:43 +0530

    git pull

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { FaBookOpen, FaUser } from "react-icons/fa";
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from "react-router-dom";
-import { selectCurrentUser, logout, login } from '../assets/redux/HomeSlice';
+import { selectCurrentUser, logout } from '../assets/redux/HomeSlice';
 import { jwtDecode } from "jwt-decode";
 
 function Header() {
@@ -10,22 +10,25 @@ function Header() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            if (decodedToken.exp * 1000 < new Date().getTime()) {
-                handleLogout();
-            } else {
-                // Dispatch login action to set the current user
-                dispatch(login(decodedToken));
-            }
-        }
-    }, [dispatch, navigate]);
+    const token = localStorage.getItem('Token');
+
+    // useEffect(() => {
+    //     const token = localStorage.getItem('Token');
+    //     if (token) {
+    //         const decodedToken = jwtDecode(token);
+    //         if (decodedToken.exp * 1000 < new Date().getTime()) {
+    //             handleLogout();
+    //         }
+    //     } else {
+    //         const profile = JSON.parse(localStorage.getItem("Profile"));
+    //         if (profile) {
+    //             dispatch(login(profile)); // Dispatch login action to set the current user
+    //         }
+    //     }
+    // }, [currentUser?.token, dispatch, navigate]); // Include currentUser?.token in the dependency array
 
     const handleLogout = () => {
-        localStorage.removeItem("token");
-        dispatch(logout());
+        localStorage.removeItem('Token');
         navigate("/");
     };
 
@@ -38,22 +41,22 @@ function Header() {
                 </div>
             </Link>
             <div className="flex gap-10 items-center absolute right-10 font-semibold font-serif text-2xl">
-                {token ? (
+                {token ? ( // Check if currentUser exists
                     <>
                         <Link to={`/user/${currentUser?.result?._id}`}>
                             <div className="bg-red-500 h-10 w-10 flex justify-center items-center rounded-full cursor-pointer">
                                 <FaUser />
                             </div>
                         </Link>
-                        <div className="font-semibold font-serif hover:text-teal-400 cursor-pointer" onClick={handleLogout}>Logout</div>
+                        <div className="font-semibold font-serif hover:text-teal-400 cursor-pointer" onClick={handleLogout}>logout</div>
                     </>
                 ) : (
                     <>
                         <Link to='/login'>
-                            <div className="font-semibold font-serif hover:text-teal-400 cursor-pointer">Login</div>
+                            <div className="font-semibold font-serif hover:text-teal-400 cursor-pointer">login</div>
                         </Link>
                         <Link to='/signup'>
-                            <div className="flex bg-neutral-700 h-10 w-[100px] text-white justify-center items-center rounded-lg hover:text-teal-400 cursor-pointer">Signup</div>
+                            <div className="flex bg-neutral-700 h-10 w-[100px] text-white justify-center items-center rounded-lg hover:text-teal-400 cursor-pointer">signup</div>
                         </Link>
                     </>
                 )}
